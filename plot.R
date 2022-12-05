4343ecfff992cc946cd9657d30d2c98a92fb0772
rturquier <remi.turquier@ensae.fr>
2022-12-05 22:12:03 +0100

    🧹 Delete scatterplot and stuff with wide df
    Approximative revert of d352183 f576363 05a240e

--- plot.R
+++ plot.R
@@ -7,10 +7,6 @@ decisions_df <- read_dta(
   file.path(datasets_path,"voxA-cr021A_v01a-DecisionsLONG.dta")
 )
 
-decisions_wide_df <- read_dta(
-  file.path(datasets_path,"voxA-cr001A_v01a-DecisionsWIDE.dta")
-)
-
 crowd_out_df <- decisions_df %>%
   filter(z %in% c(50, 74)) %>%  
   group_by(newid, GovtHigh) %>%
@@ -19,14 +15,6 @@ crowd_out_df <- decisions_df %>%
   drop_na() %>%
   mutate(relative_crowd_out = crowd_out / 6) %>%
   mutate(GovtHigh = as.factor(GovtHigh))
-
-temp_df <- decisions_wide_df %>%
-  select(newid, h04_y46, h10, h28_y46, h34)  %>%
-  mutate(crowd_out_low_external  = h04_y46 - h10,
-         crowd_out_high_external =  h28_y46 - h34,
-         relative_crowd_out_low_external  =  crowd_out_low_external / 6,
-         relative_crowd_out_high_external =  crowd_out_high_external / 6)
-
 
 decisions_df %>%
   filter(Budget %in% c(2, 5)) %>%
@@ -53,9 +41,4 @@ ggplot(crowd_out_df, aes(x = GovtHigh,
   geom_boxplot(color="#00000060", fill = "#00000000", outlier.shape = NA) +
   stat_boxplot(geom = "errorbar", alpha=0.3, width=0.25) +
   theme_minimal()
-
-
-ggplot(temp_df, aes(x = relative_crowd_out_low_external,
-                    y = relative_crowd_out_high_external)) +
-  geom_point(alpha=.2) +
-  theme_light()
+  
